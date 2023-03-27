9ce43fe30974480a671093b27ad0e28a1ffeea27
Mauritz Marais <mauritz303@gmail.com>
2023-03-27 16:46:18 +0200

    setup view engine and DB connection

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+
+const routes = require("./routes/routes");
+
+// Connect DB
+require('dotenv').config();
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_LOGIN;
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(mongoDB);
+}
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+
+app.listen(3000);
+
