5888eb4492fa671aa00af0f5d4dcf793283102db
Mauritz Marais <mauritz303@gmail.com>
2023-03-29 12:07:08 +0200

    added signup form post route

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const indexController = require('../controllers/indexController');
-const signupController = require('../controllers/signupController');
+const indexController = require("../controllers/indexController");
+const signupController = require("../controllers/signupController");
 
 // Homepage
-router.get('/', indexController.getHomePage);
+router.get("/", indexController.getHomePage);
 
 // Sign up form
-router.get('/signup', signupController.getSignupForm);
+// GET
+router.get("/signup", signupController.signupFormGet);
+
+//POST
+router.post("/signup", signupController.signupFormPost);
 
 // Login form
 
