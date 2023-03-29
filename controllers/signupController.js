0515ff0c10967b1421250ab162f26ba6d71cccd3
Mauritz Marais <mauritz303@gmail.com>
2023-03-29 12:06:12 +0200

    updated form post function to create new user and validate and sanitize form data

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,8 +1,65 @@
 const User = require("../models/user");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { body, validationResult } = require('express-validator');
+
+// setup strategy
+
 
 // sign up form GET
-exports.getSignupForm = (req, res) => {
+exports.signupFormGet = (req, res) => {
     res.render("signup_form", { title: "Sign up" })
 }
 
 // sign up form POST
+exports.signupFormPost = [
+    // validate and sanitize
+    body("firstname", "please enter your first name")
+        .isLength({ min: 1 })
+        .trim()
+        .escape(),
+    body("lastname", "please enter your last name")
+        .isLength({ min: 1 })
+        .trim()
+        .escape(),
+    body("username", "please enter your email")
+        .isEmail()
+        .normalizeEmail()
+        .escape(),
+    body("password", "please enter a password")
+        .isLength({ min: 8 })
+        .trim()
+        .escape(),
+
+    async function (req, res, next) {
+        // errors
+        const errors = validationResult(req);
+
+        const user = new User({
+            first_name: req.body.firstname,
+            last_name: req.body.lastname,
+            username: req.body.username,
+            password: req.body.password,
+            membership_status: false
+        });
+
+        if (!errors.isEmpty()) {
+            res.render("signup_form", {
+                title: "Sign up",
+                user,
+                errors: errors.array()
+            });
+            // return if there are errors
+            return;
+        }
+        // create new user
+        try {
+            const result = await user.save();
+            res.redirect('/');
+        }
+        catch (err) {
+            return next(err);
+        }
+    }
+];
