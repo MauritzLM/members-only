602d0596cfa7182856f71e01fbde3d6be64efc82
Mauritz Marais <mauritz303@gmail.com>
2023-03-29 16:06:01 +0200

    added local srategy and serialize/deserialize functions

--- app.js
+++ app.js
@@ -32,10 +32,49 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
 
+// authentication strategy
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            };
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    // passwords match! log user in
+                    return done(null, user)
+                } else {
+                    // passwords do not match!
+                    return done(null, false, { message: "Incorrect password" })
+                }
+            })
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        };
+    })
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    };
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+
+
 
 
 app.listen(3000);
