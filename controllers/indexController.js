f918e65f88b7196a8afba12bdb15b942613a47c0
Mauritz Marais <mauritz303@gmail.com>
2023-04-05 14:55:07 +0200

    changed get function to find all messages and pass them to the view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,18 @@
-
+const Message = require("../models/message");
 
 // Display home page on get
-exports.getHomePage = (req, res, next) => {
-    res.render("index", { title: "Homepage" });
+exports.getHomePage = async function (req, res, next) {
+    // GET all messages
+    try {
+        const results = await Message.find().populate("author");
+
+        res.render("index", {
+            title: "Homepage",
+            messages: results
+        });
+
+    }
+    catch (err) {
+        return next(err);
+    }
 }
