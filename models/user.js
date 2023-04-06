8fa16c424bec4d991cbb67e62132cbb4c71b930c
Mauritz Marais <mauritz303@gmail.com>
2023-04-06 14:24:05 +0200

    added admin field

--- models/user.js
+++ models/user.js
@@ -6,8 +6,8 @@ const UserSchema = new Schema({
     last_name: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    membership_status: { type: Boolean, default: false, required: true }
+    membership_status: { type: Boolean, default: false, required: true },
+    admin: { type: Boolean, default: false }
 });
-
 
 module.exports = mongoose.model('User', UserSchema);
